58dd9788868815548f912d2d7074829b2ba9964b
borda <bordonign@gmail.com>
2024-11-11 22:43:53 -0300

    Add website files, first version

---
+++ hamburger-menu.js
@@ -0,0 +1,9 @@
+// JavaScript para el menú hamburguesa
+function myFunction() {
+    var x = document.getElementById("hamburger-nav-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
